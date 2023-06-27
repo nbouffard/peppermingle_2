b125bb00087494a71d5c265e19771e2aafb814df
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-06-27 13:09:05 +0200

    update model validates and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
+  has_many :recipes, dependent: :destroy
+  has_many :events, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+
+  validates :user_name, uniqueness: true
+  validate :date_of_birth_greater_than_18_years
+
+  private
+
+  def date_of_birth_greater_than_18_years
+    if date_of_birth.present? && date_of_birth > 18.years.ago.to_date
+      errors.add(:date_of_birth, 'must be at least 18 years ago')
+    end
+  end
 end
