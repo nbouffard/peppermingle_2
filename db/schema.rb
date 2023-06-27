50b4c074fabf73a9ec23c3b6e128a2e942faccdc
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-06-27 12:00:25 +0200

    add ingredient model/migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_27_095548) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_27_095933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "amount"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
