f67fab0b958fdda476a6862f41743600958bb68b
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-06-27 12:08:41 +0200

    add default value to paid in events

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_27_100355) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_27_100726) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "event_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_bookings_on_event_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.date "date"
-    t.boolean "paid"
+    t.boolean "paid", default: false
     t.bigint "user_id", null: false
     t.bigint "recipe_id", null: false
     t.datetime "created_at", null: false
@@ -80,6 +89,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_100355) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "events"
+  add_foreign_key "bookings", "users"
   add_foreign_key "events", "recipes"
   add_foreign_key "events", "users"
   add_foreign_key "ingredient_join_tables", "ingredients"
