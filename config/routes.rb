0807a2637a3e9f4c38b809d90f187ab9f12e8353
Quan <99978041+ShimaGunz@users.noreply.github.com>
2023-06-28 11:22:55 +0100

    Create/Destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :events
   resources :recipes, only: %i[index new show create edit update]
   resources :users, only: %i[show edit update]
+
+  resources :events do
+    resources :bookings, only: %i[new create]
+  end
+
+  get 'my_bookings', to: 'bookings#my_bookings', as: 'my_bookings'
+
+  resources :bookings, only: %i[show destroy]
+
 end
