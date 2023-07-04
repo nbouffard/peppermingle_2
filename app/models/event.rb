52af32a6fd5c0f99a40287622e010ff84d50fc28
igor dinic <dinicigor84@gmail.com>
2023-07-04 20:30:02 +0200

    added callback # event_chatroom in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,11 @@ class Event < ApplicationRecord
   has_many_attached :photos
   has_many :users, through: :bookings
   has_one :chatroom
+  after_save :event_chatroom
+
+  private
+
+  def event_chatroom
+    Chatroom.create(name: title, event: self)
+  end
 end
