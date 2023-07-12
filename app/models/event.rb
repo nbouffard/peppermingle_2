42fdad22b5445737ddaefb83a174da0337666fd0
Prakashini Murugan <prakashini.murugan@outlook.com>
2023-07-12 19:47:35 +0200

    added dependent destroy to message and chatroom

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ApplicationRecord
   has_many_attached :photos
   has_one :room_url
   has_many :users, through: :bookings
-  has_one :chatroom
+  has_one :chatroom, dependent: :destroy
   after_save :event_chatroom
 
   def self.filter_by_date(date)
