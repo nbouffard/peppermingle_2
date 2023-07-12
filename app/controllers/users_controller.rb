9590c9ea8d191c6962984df39106726292d9a04e
igor dinic <dinicigor84@gmail.com>
2023-07-12 21:34:28 +0200

    added chatroom to user hub

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,29 +37,13 @@ class UsersController < ApplicationController
         format.html
         format.text { render partial: 'bookings/booking_content', locals: { bookings: @bookings }, formats: [:html] }
       end
+    when 'chatrooms'
+      @chatrooms = Chatroom.all
+      respond_to do |format|
+        format.html
+        format.text { render partial: 'chatrooms/chats', locals: { chatrooms: @chatrooms }, formats: [:html] }
+      end
     end
-
-
-    # if params[:query] == 'recipes'
-    #   @recipes = policy_scope(Recipe)
-
-    #   respond_to do |format|
-    #     format.html
-    #     format.text { render partial: 'recipes/content', locals: { recipes: @recipes }, formats: [:html] }
-    #   end
-    #   elsif params[:query] == 'events'
-    #     @events = policy_scope(Event)
-    #     respond_to do |format|
-    #       format.html
-    #       format.text { render partial: 'events/event_content', locals: { events: @events }, formats: [:html] }
-    #     end
-    #   elsif params[:query] == 'bookings'
-    #     @bookings = policy_scope(Booking)
-    #     respond_to do |format|
-    #       format.html
-    #       format.text { render partial: 'bookings/booking_content', locals: { bookings: @bookings }, formats: [:html] }
-    #     end
-    # end
   end
 
   private
